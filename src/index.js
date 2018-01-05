c60cbb6b4281671662a6bbadd964f8d79a12d7d3
Jeferson Rubert <jrubert@xmatters.com>
2018-01-05 11:41:53 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+const API_KEY = 'AIzaSyDTT9o6F1xF9ZsKskC-qclpKUi6yCcIQho';
+
+ReactDOM.render( <App />, document.querySelector('.container'));
