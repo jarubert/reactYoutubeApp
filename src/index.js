e877183caddc53ed4711f51334beabdec22df9d8
Jeferson Rubert <jrubert@xmatters.com>
2018-01-08 09:19:38 -0800

    final youtube application

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
-const API_KEY = 'AIzaSyDTT9o6F1xF9ZsKskC-qclpKUi6yCcIQho';
 
 ReactDOM.render( <App />, document.querySelector('.container'));
